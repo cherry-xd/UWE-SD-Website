4d85bb45fbdfd199abc3632ebb6cb50071e99bcc
Luke Walker <lukeww101@gmail.com>
2022-04-28 02:32:45 +0100

    added ratings page

--- src/components/Ratings.jsx
+++ src/components/Ratings.jsx
@@ -8,12 +8,206 @@ function Ratings() {
           <div class="col"/>
           <div class="col-9">
             <h1 class="font-weight-light text-center">Ratings</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
+            <table class="table text-center">
+              <tr>
+                <th>Men</th>
+                <th>Points</th>
+                <th>Women</th>
+                <th>Points</th>
+              </tr>
+              <tr>
+                <td>1st: MP15 🥇</td>
+                <td>361.5</td>
+                <td>1st: FP07 🥇</td>
+                <td>436.5</td>
+              </tr>
+              <tr>
+                <td>2nd: MP07 🥈</td>
+                <td>350</td>
+                <td>2nd: FP26 🥈</td>
+                <td>348</td>
+              </tr>
+              <tr>
+                <td>3rd: MP14 🥉</td>
+                <td>312.5</td>
+                <td>3rd: FP21 🥉</td>
+                <td>325</td>
+              </tr>
+              <tr>
+                <td>4th: MP25</td>
+                <td>310</td>
+                <td>4th: FP19</td>
+                <td>310</td>
+              </tr>
+              <tr>
+                <td>5th: MP13</td>
+                <td>213.25</td>
+                <td>5th: FP03</td>
+                <td>259.75</td>
+              </tr>
+              <tr>
+                <td>6th: MP22</td>
+                <td>205</td>
+                <td>6th: FP10</td>
+                <td>194.5</td>
+              </tr>
+              <tr>
+                <td>7th: MP23</td>
+                <td>178.25</td>
+                <td>7th: FP02</td>
+                <td>192</td>
+              </tr>
+              <tr>
+                <td>8th: MP11</td>
+                <td>178</td>
+                <td>8th: FP01</td>
+                <td>167</td>
+              </tr>
+              <tr>
+                <td>9th: MP20</td>
+                <td>122.5</td>
+                <td>9th: FP16</td>
+                <td>118</td>
+              </tr>
+              <tr>
+                <td>10th: MP30</td>
+                <td>122.5</td>
+                <td>10th: FP04</td>
+                <td>113</td>
+              </tr>
+              <tr>
+                <td>11th: MP05</td>
+                <td>115</td>
+                <td>11th: FP22</td>
+                <td>108</td>
+              </tr>
+              <tr>
+                <td>12th: MP02</td>
+                <td>108.75</td>
+                <td>12th: FP25</td>
+                <td>100.75</td>
+              </tr>
+              <tr>
+                <td>13th: MP32</td>
+                <td>100.75</td>
+                <td>13th: FP32</td>
+                <td>86.5</td>
+              </tr>
+              <tr>
+                <td>14th: MP17</td>
+                <td>97.5</td>
+                <td>14th: FP29</td>
+                <td>81.75</td>
+              </tr>
+              <tr>
+                <td>15th: MP29</td>
+                <td>69</td>
+                <td>15th: FP31</td>
+                <td>69</td>
+              </tr>
+              <tr>
+                <td>16th: MP27</td>
+                <td>58.75</td>
+                <td>16th: FP28</td>
+                <td>52.75</td>
+              </tr>
+              <tr>
+                <td>17th: MP03</td>
+                <td>58</td>
+                <td>17th: FP14</td>
+                <td>46</td>
+              </tr>
+              <tr>
+                <td>18th: MP28</td>
+                <td>52</td>
+                <td>18th: FP18</td>
+                <td>42.5</td>
+              </tr>
+              <tr>
+                <td>19th: MP21</td>
+                <td>48</td>
+                <td>19th: FP20</td>
+                <td>38.5</td>
+              </tr>
+              <tr>
+                <td>20th: MP12</td>
+                <td>46</td>
+                <td>20th: FP24</td>
+                <td>38.5</td>
+              </tr>
+              <tr>
+                <td>21st: MP18</td>
+                <td>46</td>
+                <td>21st: FP15</td>
+                <td>29</td>
+              </tr>
+              <tr>
+                <td>22nd: MP26</td>
+                <td>41.25</td>
+                <td>22nd: FP13</td>
+                <td>27</td>
+              </tr>
+              <tr>
+                <td>23rd: MP19</td>
+                <td>31.75</td>
+                <td>23rd: FP05</td>
+                <td>16.25</td>
+              </tr>
+              <tr>
+                <td>24th: MP06</td>
+                <td>31</td>
+                <td>24th: FP06</td>
+                <td>16.25</td>
+              </tr>
+              <tr>
+                <td>25th: MP01</td>
+                <td>29.75</td>
+                <td>25th: FP09</td>
+                <td>16.25</td>
+              </tr>
+              <tr>
+                <td>26th: MP16</td>
+                <td>29</td>
+                <td>25th: FP27</td>
+                <td>16.25</td>
+              </tr>
+              <tr>
+                <td>27th: MP31</td>
+                <td>15.5</td>
+                <td>27th: FP12</td>
+                <td>15.5</td>
+              </tr>
+              <tr>
+                <td>28th: MP09</td>
+                <td>11.5</td>
+                <td>28th: FP23</td>
+                <td>13.5</td>
+              </tr>
+              <tr>
+                <td>29th: MP04</td>
+                <td>0</td>
+                <td>29th: FP08</td>
+                <td>0</td>
+              </tr>
+              <tr>
+                <td>30th: MP08</td>
+                <td>0</td>
+                <td>30th: FP11</td>
+                <td>0</td>
+              </tr>
+              <tr>
+                <td>31st: MP10</td>
+                <td>0</td>
+                <td>31st: FP17</td>
+                <td>0</td>
+              </tr>
+              <tr>
+                <td>32nd: MP24</td>
+                <td>0</td>
+                <td>32nd: FP30</td>
+                <td>0</td>
+              </tr>
+            </table>
           </div>
           <div class="col"/>
         </div>
